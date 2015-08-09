e2631f3e664d146922685c045328f2fdbf391c19
Mao <asdm0524@gmail.com>
2015-08-09 21:28:39 +0900

    add associations

--- PoemsGithub/app/models/owner.rb
+++ PoemsGithub/app/models/owner.rb
@@ -1,3 +1,4 @@
 class Owner < ActiveRecord::Base
+  belong_to :users
   belong_to :poems
 end
--- PoemsGithub/app/models/poem.rb
+++ PoemsGithub/app/models/poem.rb
@@ -1,3 +1,5 @@
 class Poem < ActiveRecord::Base
-  belong to :owners
+  belong_to :owners
+  belong_to :genres
+  has_many :stars
 end
