cb5f8b3d5ed846283541f15fb9fd40ef51530c91
hanzhnagtitech <han.zhang@techouse.jp>
2015-08-10 12:11:56 +0900

    version controll at show now work

--- PoemsGithub/app/controllers/poems_controller.rb
+++ PoemsGithub/app/controllers/poems_controller.rb
@@ -1,4 +1,5 @@
 class PoemsController < ApplicationController
+require 'time'
 
   def new
     @is_edit = params[:is_edit].to_s
@@ -34,8 +35,8 @@ class PoemsController < ApplicationController
     @poem_logs = Poem.where(:title => @poem.title)
     @user = User.where(:id => @poem.owner_id).first
     @versions = @poem_logs.order("created_at desc").pluck(:created_at)
-    if params[:created_at]
-      @poem = @poem_logs.where(:created_at => params[:created_at]).first
+    if params[:poem]
+      @poem = @poem_logs.where(:id => params[:poem][:id]).first
     end
   end
 
